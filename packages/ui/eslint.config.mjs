643bb7f3c303133337642f7faaf19fb731f3009c
Craig McClanahan <craigmcc@gmail.com>
2025-04-17 11:41:26 -0700

    Forgot eslint.config.mjs files.

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config } from "@craigmcc/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
