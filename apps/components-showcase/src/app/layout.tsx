03215c6804b68df042f74064628c546028b0a397
Craig McClanahan <craigmcc@gmail.com>
2025-04-23 14:03:42 -0700

    Resolve import issues with a correction to the exports in daisyui-components.

--- apps/components-showcase/src/app/layout.tsx
+++ apps/components-showcase/src/app/layout.tsx
@@ -10,8 +10,8 @@ import { Inter } from "next/font/google";
 // Internal Modules ----------------------------------------------------------
 
 import "./globals.css";
-//import { ThemeContextProvider } from "@craigmcc/daisyui-components/ThemeContext";
-//import { ThemeWrapper } from "@craigmcc/daisyui-components/ThemeWrapper";
+import { ThemeContextProvider } from "@craigmcc/daisyui-components/ThemeContext";
+import { ThemeWrapper } from "@craigmcc/daisyui-components/ThemeWrapper";
 
 // Public Objects ------------------------------------------------------------
 
@@ -22,26 +22,26 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-      {/*<ThemeContextProvider>*/}
-      {/*  <ThemeWrapper>*/}
-      {/*  TODO - this will get replaced by the nav bar with dropdowns */}
-          <div className="navbar bg-base-200">
-            <div className="flex-1">
-              <a className="btn btn-ghost normal-case text-xl">Components Showcase</a>
-            </div>
+    <body className={inter.className}>
+    <ThemeContextProvider>
+      <ThemeWrapper>
+        {/*  TODO - this will get replaced by the nav bar with dropdowns */}
+        <div className="navbar bg-base-200">
+          <div className="flex-1">
+            <a className="btn btn-ghost normal-case text-xl">Components Showcase</a>
           </div>
-          {children}
-      {/*  </ThemeWrapper>*/}
-      {/*</ThemeContextProvider>*/}
+        </div>
         {children}
-      </body>
+      </ThemeWrapper>
+    </ThemeContextProvider>
+    {children}
+    </body>
     </html>
   );
 }
