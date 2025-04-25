3064af14c5926b1a924180216e6d66813cb68532
Craig McClanahan <craigmcc@gmail.com>
2025-04-25 15:08:17 -0700

    Switch ThemeSwitcher to use generic Dropdown component.  Currently leaves the themes dropdown open after selection.

--- packages/daisyui-components/src/ThemeSwitcher.tsx
+++ packages/daisyui-components/src/ThemeSwitcher.tsx
@@ -10,37 +10,10 @@ import { useContext } from "react";
 
 // Internal Modules ----------------------------------------------------------
 
+import { Dropdown } from "@craigmcc/daisyui-components/Dropdown";
 import { ThemeContext } from "./ThemeContext";
 
-// Public Objects ------------------------------------------------------------
-
-export const ThemeSwitcher = () => {
-
-  const { changeTheme, theme } = useContext(ThemeContext);
-
-  const capitalize = (word: string) => {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  }
-
-  return (
-    <div className="dropdown dropdown-end">
-      <details>
-        <summary className="btn btn-ghost z-1">Theme</summary>
-        <ul tabIndex={0} className="menu dropdown-content z-1 mt-6 shadow-sm bg-base-300 box">
-          {THEMES.map((THEME) => (
-            <li
-              className={THEME === theme ? "font-bold" : "font-normal"}
-              key={THEME}
-              onClick={() => changeTheme(THEME)}
-            >
-              {capitalize(THEME)}
-            </li>
-          ))}
-        </ul>
-      </details>
-    </div>
-  )
-}
+// Private Objects -----------------------------------------------------------
 
 // Keep this list in sync with the themes listed for the daisyUI plugin in globals.css
 const THEMES = [
@@ -77,3 +50,49 @@ const THEMES = [
   "winter",
   // "wireframe",
 ];
+
+type OPTION = {
+  // The label for this dropdown detail.
+  label: string;
+  // The theme we should switch to when this option is selected.
+  theme: string;
+}
+
+const capitalize = (word: string) => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+const OPTIONS: OPTION[] = [];
+THEMES.forEach((option) => {
+  OPTIONS.push({ label: capitalize(option), theme: option});
+});
+
+// Public Objects ------------------------------------------------------------
+
+export const ThemeSwitcher = () => {
+
+  const { changeTheme } = useContext(ThemeContext);
+
+  function handleClick(detail: string) {
+    const option = OPTIONS.find((option) => option.label === detail);
+    if (option) {
+//      console.log(`ThemeSwitcher: changing theme to: ${detail}`);
+      changeTheme(option.theme);
+    } else {
+      console.error(`ThemeSwitcher: No option found for detail: ${detail}`);
+    }
+  }
+
+  return (
+    <Dropdown
+      className="dropdown-end"
+      classNameDetails="z-1 mt-6 shadow-sm bg-base-300 box"
+      closeOnSelect={false}
+      details={OPTIONS.map((option) => option.label)}
+      handleClick={handleClick}
+      name="theme-switcher"
+      summary="Theme"
+    />
+  )
+
+}
