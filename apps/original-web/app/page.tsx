2c363901af3a7a3acc5f6efa1d6225bd2f419315
Craig McClanahan <craigmcc@gmail.com>
2025-05-01 11:37:00 -0700

    Move templated apps and package to original-docs, original-web, and original-ui.

--- apps/original-web/app/page.tsx
+++ apps/original-web/app/page.tsx
@@ -1,5 +1,5 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@craigmcc/ui/button";
+import { Button } from "@craigmcc/original-ui/button";
 import styles from "./page.module.css";
 import { JSX } from "react";
 
