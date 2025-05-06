2ac33ce970787697bc2d29b6ebe88112ff1b07bf
Craig McClanahan <craigmcc@gmail.com>
2025-05-06 11:43:19 -0700

    Shell of new apps/bootstrap-showcase to feature React Bootstrap based components.

---
+++ apps/bootstrap-showcase/src/app/layout.tsx
@@ -0,0 +1,43 @@
+/**
+ * Global layout for the application.
+ */
+
+// External Modules ----------------------------------------------------------
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+//import { ToastContainer } from "react-toastify";
+
+// Internal Modules ----------------------------------------------------------
+
+import "./globals.css";
+//import { NavBar } from "@/components/layout/NavBar";
+//import { Providers } from "@/components/layout/Providers";
+
+// Public Objects ------------------------------------------------------------
+
+const inter = Inter({ subsets: ["latin"] });
+export const metadata: Metadata = {
+  description: "Demonstration of React Boostrap and Tanstack Form components",
+  title: "Bootstrap Showcase",
+};
+
+export default function RootLayout({
+                                     children,
+                                   }: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+    <body className={inter.className}>
+    {/*
+    <Providers>
+      <NavBar/>
+      {children}
+    </Providers>
+*/}
+    {children}
+    </body>
+    </html>
+  );
+}
---
+++ apps/bootstrap-showcase/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <p>Components Showcase</p>
+      <p>Select the component you wish to view from one of the menu dropdowns.</p>
+      <p>Demonstration of ShadCN UI and Tanstack Form components</p>
+    </main>
+  );
+}
