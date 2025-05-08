81e5b82cf41bcf3535adb85cbb6cb7c0b8c500f6
Craig McClanahan <craigmcc@gmail.com>
2025-05-08 09:56:29 -0700

    (in apps/bootstrap-example) Sample MenuBar component.

--- apps/bootstrap-example/src/app/layout.tsx
+++ apps/bootstrap-example/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {MenuBar} from "@/components/layout/MenuBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <MenuBar/>
         {children}
       </body>
     </html>
