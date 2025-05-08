ac2a14848acde941e78571094b71607b48688791
Craig McClanahan <craigmcc@gmail.com>
2025-05-08 14:58:03 -0700

    Rename bootstrap-example to @craigmcc/bootstrap-showcase, get rid of old bootstrap-showcase.

--- bootstrap-showcase/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <p>Components Showcase</p>
-      <p>Select the component you wish to view from one of the menu dropdowns.</p>
-      <p>Demonstration of React Bootstrap and Tanstack Form components</p>
-    </main>
-  );
-}
