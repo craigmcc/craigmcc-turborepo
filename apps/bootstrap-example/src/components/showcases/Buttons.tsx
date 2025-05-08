82285df064e04c4a320b99e98edf8e79aadb6ef7
Craig McClanahan <craigmcc@gmail.com>
2025-05-08 13:10:10 -0700

    Clean up some changes to Bootstrap 5 classnames

--- apps/bootstrap-example/src/components/showcases/Buttons.tsx
+++ apps/bootstrap-example/src/components/showcases/Buttons.tsx
@@ -7,28 +7,28 @@ const ButtonsShowcase: React.FC = () => (
     <Button variant="primary" className="me-2">
       Primary
     </Button>
-    <Button variant="secondary" className="me-1">
+    <Button variant="secondary" className="me-2">
       Secondary
     </Button>
-    <Button variant="success" className="me-1">
+    <Button variant="success" className="me-2">
       Success
     </Button>
-    <Button variant="warning" className="me-1">
+    <Button variant="warning" className="me-2">
       Warning
     </Button>
-    <Button variant="danger" className="me-1">
+    <Button variant="danger" className="me-2">
       Danger
     </Button>
-    <Button variant="info" className="me-1">
+    <Button variant="info" className="me-2">
       Info
     </Button>
-    <Button variant="light" className="me-1">
+    <Button variant="light" className="me-2">
       Light
     </Button>
-    <Button variant="dark" className="me-1">
+    <Button variant="dark" className="me-2">
       Dark
     </Button>
-    <Button variant="link" className="me-1">
+    <Button variant="link" className="me-2">
       Link
     </Button>
   </div>
--- apps/bootstrap-example/src/components/showcases/Toasts.tsx
+++ apps/bootstrap-example/src/components/showcases/Toasts.tsx
@@ -11,12 +11,10 @@ const ToastsShowcase: React.FC = () => {
   return (
     <>
       {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
-      {/*
-    // @ts-ignore */}
       <Toast show={show} onClose={() => toggleShow(false)}>
         <Toast.Header>
-          <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
-          <strong className="mr-auto">Bootstrap</strong>
+          <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+          <strong className="me-auto">Bootstrap</strong>
           <small>11 mins ago</small>
         </Toast.Header>
         <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
