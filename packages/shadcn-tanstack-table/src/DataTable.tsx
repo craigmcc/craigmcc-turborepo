546ee9daa17220fa9eb39114448614857cc0e399
Craig McClanahan <craigmcc@gmail.com>
2025-10-30 15:58:40 -0700

    Clean out obsolete commented out code in DataTable.

--- packages/shadcn-tanstack-table/src/DataTable.tsx
+++ packages/shadcn-tanstack-table/src/DataTable.tsx
@@ -43,22 +43,8 @@ import {
 
 // Public Objects ------------------------------------------------------------
 
-/*
-export type MutationFormProps<TData> = {
-  // The data model we are editing or removing (or null if creating)
-  data: TData | null;
-  // Are we removing (true) or creating/updating (false)?
-  isRemoving: boolean | null;
-  // A cleanup function to call when the mutation is complete
-  // (either successfully or not)
-  onComplete?: () => void;
-}
-*/
-
 type DataTableProps<TData> = {
-  // React component that implements the CRUD behaviors for TData
-//  mutators?: (props: MutationFormProps<TData>) => React.JSX.Element | undefined;
-  // Show pagination controls
+  // Show pagination controls [false]
   showPagination?: boolean;
   // The Tanstack Table we are displaying
   table: TanstackTable<TData>,
@@ -74,78 +60,22 @@ export function DataTable<TData>(
   const pageCount = table.getPageCount();
 
   return (
-    <>
+    <Table className="border-4 rounded-md">
 
-      {/*
-      {showModal && mutators && (creatingRow || updatingRow || removingRow) && (
-        <dialog className="modal modal-open">
-          <div className="modal-box">
-            <div className="flex flex-row w-full justify-end">
-              <span className="tooltip tooltip-left" data-tip="Close" >
-              <button
-                className="btn btn-sm btn-ghost justify-end"
-                onClick={() => {
-                  setCreatingRow(null);
-                  setUpdatingRow(null);
-                  setRemovingRow(null);
-                  setShowModal(false);
-                  // No revalidate on modal close without a mutation
-                }}
-              >
-                <X size={16}/>
-              </button>
-              </span>
-            </div>
-            {createElement(mutators, {
-              data: updatingRow || removingRow, // NOTE: not passing creatingRow
-              isRemoving: !!removingRow,
-              onComplete: onComplete,
-            })}
-          </div>
-        </dialog>
-      )}
-*/}
-
-      {/*
-      {mutators && (supportsCreating || title) && (
-
-        <div className={clsx(titleClassName, "p-2")}>
-         {title && (
-            <span>{title}</span>
-          )}
-          {supportsCreating && (
-            <span className="tooltip tooltip-right" data-tip="Add">
-          <button
-            className="btn btn-primary btn-sm"
-            onClick={() => {
-              setCreatingRow({} as TData);
-              setShowModal(true);
-            }}
-          >
-            <Plus size={16}/>
-          </button>
-          </span>
-          )}
-        </div>
-      )}
-*/}
-
-      <Table className="border border-4 rounded-md">
-
-        <TableHeader>
-          {table.getHeaderGroups().map(headerGroup => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map(header => (
-                <TableHead key={header.id} colSpan={header.colSpan}>
-                  <div  className="flex flex-row w-full justify-center">
-                    {flexRender(header.column.columnDef.header, header.getContext())}
-                    { header.column.getCanSort() ? (
-                        <>
-                          <span>&nbsp;</span>
-                    <span
-                      onClick={header.column.getToggleSortingHandler()}
-                      style={{ cursor: "pointer" }}
-                    >
+      <TableHeader>
+        {table.getHeaderGroups().map(headerGroup => (
+          <TableRow key={headerGroup.id}>
+            {headerGroup.headers.map(header => (
+              <TableHead key={header.id} colSpan={header.colSpan}>
+                <div  className="flex flex-row w-full justify-center">
+                  {flexRender(header.column.columnDef.header, header.getContext())}
+                  { header.column.getCanSort() ? (
+                      <>
+                        <span>&nbsp;</span>
+                        <span
+                          onClick={header.column.getToggleSortingHandler()}
+                          style={{ cursor: "pointer" }}
+                        >
                       {header.column.getIsSorted() === "asc" ? (
                         <ArrowUpAZ className="text-info" size={24}/>
                       ) : header.column.getIsSorted() === "desc" ? (
@@ -154,96 +84,94 @@ export function DataTable<TData>(
                         <ArrowDownUp className="text-info" size={24}/>
                       )}
                     </span>
-                        </>
-                      ) :
-                      null
-                    }
-                  </div>
-                </TableHead>
-              ))}
-            </TableRow>
-          ))}
-        </TableHeader>
-
-        <TableBody>
-          {table.getRowModel().rows.map(row => (
-            <TableRow key={row.id} className="odd:bg-muted/50 odd:hover:bg-muted/50">
-              {row.getVisibleCells().map(cell => (
-                <TableCell key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableBody>
-
-        { showPagination ? (
-          <TableFooter>
-            <TableRow>
-              <TableHead colSpan={table.getCenterLeafColumns().length}>
-                <div className="text-center mt-2 flex flex-row justify-center items-center gap-2">
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <button
-                        disabled={!table.getCanPreviousPage()}
-                        onClick={() => table.firstPage()}>
-                        <ArrowLeftToLine/>
-                      </button>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      First Page
-                    </TooltipContent>
-                  </Tooltip>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <button
-                        disabled={!table.getCanPreviousPage()}
-                        onClick={() => table.previousPage()}>
-                        <ArrowLeft/>
-                      </button>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      Previous Page
-                    </TooltipContent>
-                  </Tooltip>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <button
-                        disabled={!table.getCanNextPage()}
-                        onClick={() => table.nextPage()}>
-                        <ArrowRight/>
-                      </button>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      Next Page
-                    </TooltipContent>
-                  </Tooltip>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <button
-                        disabled={!table.getCanNextPage()}
-                        onClick={() => table.lastPage()}>
-                        <ArrowRightToLine/>
-                      </button>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      Last Page
-                    </TooltipContent>
-                  </Tooltip>
-                  <span className="p-4">
-                    Page {table.getState().pagination.pageIndex + 1} of{" "}
-                        {pageCount > 0 ? pageCount : `1`}{" "}| Total of{" "}
-                        {table.getRowCount().toLocaleString()} Rows
-                  </span>
+                      </>
+                    ) :
+                    null
+                  }
                 </div>
               </TableHead>
-            </TableRow>
-          </TableFooter>
-        ) : null }
+            ))}
+          </TableRow>
+        ))}
+      </TableHeader>
 
-      </Table>
+      <TableBody>
+        {table.getRowModel().rows.map(row => (
+          <TableRow key={row.id} className="odd:bg-muted/50 odd:hover:bg-muted/50">
+            {row.getVisibleCells().map(cell => (
+              <TableCell key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
 
-    </>
+      { showPagination ? (
+        <TableFooter>
+          <TableRow>
+            <TableHead colSpan={table.getCenterLeafColumns().length}>
+              <div className="text-center mt-2 flex flex-row justify-center items-center gap-2">
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <button
+                      disabled={!table.getCanPreviousPage()}
+                      onClick={() => table.firstPage()}>
+                      <ArrowLeftToLine/>
+                    </button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    First Page
+                  </TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <button
+                      disabled={!table.getCanPreviousPage()}
+                      onClick={() => table.previousPage()}>
+                      <ArrowLeft/>
+                    </button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    Previous Page
+                  </TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <button
+                      disabled={!table.getCanNextPage()}
+                      onClick={() => table.nextPage()}>
+                      <ArrowRight/>
+                    </button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    Next Page
+                  </TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <button
+                      disabled={!table.getCanNextPage()}
+                      onClick={() => table.lastPage()}>
+                      <ArrowRightToLine/>
+                    </button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    Last Page
+                  </TooltipContent>
+                </Tooltip>
+                <span className="p-4">
+                    Page {table.getState().pagination.pageIndex + 1} of{" "}
+                  {pageCount > 0 ? pageCount : `1`}{" "}| Total of{" "}
+                  {table.getRowCount().toLocaleString()} Rows
+                  </span>
+              </div>
+            </TableHead>
+          </TableRow>
+        </TableFooter>
+      ) : null }
+
+    </Table>
 
   );
 
